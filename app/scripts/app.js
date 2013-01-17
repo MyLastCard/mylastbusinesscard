67e82d6a4cfdae65b201e52cc1d0e849c311bd9d
Blaise Freeman <blaise.freeman@gmail.com>
2013-01-17 13:49:36 -0500

    import completed prototype

---
+++ app/scripts/app.js
@@ -0,0 +1,30 @@
+
+$(document).ready(function() {
+    
+  // accordion
+  $("section.open article").slideDown();
+  $("section.accordion h1").click(function(e){
+    var $section = $(this).parent('.accordion');
+    if (!$section.hasClass('open')) {
+      $(".accordion.open").removeClass('open').find("article").slideUp();
+      $(this).parent('section').addClass('open').find("article").slideDown();      
+    }
+  });
+  
+  // carousel
+  $('#content-slider-1').royalSlider({
+    autoHeight: true,
+    arrowsNav: false,
+    fadeinLoadedSlide: false,
+    controlNavigationSpacing: 0,
+    controlNavigation: 'bullets',
+    imageScaleMode: 'none',
+    imageAlignCenter:false,
+    loop: true,
+    loopRewind: false,
+    numImagesToPreload: 6,
+    keyboardNavEnabled: true,
+    usePreloader: false
+  });
+  
+});
